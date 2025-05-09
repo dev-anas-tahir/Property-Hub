d149fc1199e2df76e680c3bdf29f06c0dc6fd3cc
Anas Tahir - Devsinc <anas.waleed@devsinc.com>
2025-05-09 21:31:15 +0500

    chore: created the django project and setup the django-environ and pipenv for packages. also restructured the project for better readability and maintainence

---
+++ config/settings/development.py
@@ -0,0 +1,5 @@
+"""Development settings for the Property-Hub project."""
+
+from config.settings.base import *
+
+DEBUG = True
---
+++ config/settings/production.py
@@ -0,0 +1,6 @@
+"""Production settings for the Property-Hub project."""
+
+from config.settings.base import *
+
+DEBUG = False
+# ALLOWED_HOSTS = ['your-production-domain.com']
