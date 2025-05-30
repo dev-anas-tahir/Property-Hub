9f1ca567815d015b3d458da886914eedda70988a
anasengence <anas.waleed@devsinc.com>
2025-05-30 21:49:04 +0500

    update: conftest.py updated

--- conftest.py
+++ conftest.py
@@ -1,5 +1,10 @@
-# conftest.py
-from pytest_factoryboy import register
+import pytest
 from apps.users.tests.factories import UserFactory
 
-register(UserFactory)
+@pytest.fixture
+def user_factory():
+    return UserFactory
+
+@pytest.fixture
+def user(db, user_factory):
+    return user_factory()
