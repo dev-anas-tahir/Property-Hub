882f3d84c8ed5985f916c42bcacab7e4acc42d23
anasengence <anas.waleed@devsinc.com>
2025-06-11 22:24:39 +0500

    update(ui): fixed ui errors

--- apps/users/templatetags/form_filters_users.py
+++ apps/users/templatetags/form_filters_users.py
@@ -3,6 +3,7 @@ This module contains template filters for form fields.
 """
 
 from django import template
+from django.forms import CheckboxInput, RadioSelect
 
 register = template.Library()
 
@@ -10,13 +11,37 @@ register = template.Library()
 def add_class(field, css_class):
     """
     Adds a CSS class to the widget of a form field.
-    Usage: {{ field|add_class:"form-control" }}
-    """
-    if not hasattr(field, 'field'):
-        return field
+    
+    Args:
+        field: The form field to add the class to
+        css_class (str): The CSS class(es) to add
         
-    if 'class' in field.widget.attrs:
-        field.widget.attrs['class'] = f"{field.widget.attrs.get('class', '')} {css_class}"
-    else:
-        field.widget.attrs['class'] = css_class
+    Returns:
+        The field with the updated widget attributes
+        
+    Usage:
+        {{ field|add_class:"form-control" }}
+        {{ field|add_class:"form-check-input" }}
+    """
+    if not field:
+        return ''
+        
+    # Handle both BoundField and widget directly
+    widget = field.field.widget if hasattr(field, 'field') else field
+    
+    # Handle different widget types
+    if isinstance(widget, (CheckboxInput, RadioSelect)):
+        # For checkboxes and radio buttons, we might want different handling
+        css_class = f'form-check-input {css_class}'
+    
+    # Handle existing classes
+    attrs = widget.attrs
+    existing_class = attrs.get('class', '').split()
+    
+    # Add new class if not already present
+    for cls in css_class.split():
+        if cls not in existing_class:
+            existing_class.append(cls)
+    
+    attrs['class'] = ' '.join(existing_class).strip()
     return field
--- config/settings.py
+++ config/settings.py
@@ -76,7 +76,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATIC_URL = "/staticfiles/"
+STATIC_URL = "/static/"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
